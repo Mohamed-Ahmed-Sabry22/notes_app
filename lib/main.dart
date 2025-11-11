f701c8130f0bd719a9f658e745ccac0eaf505332
Mohamed-Ahmed-Sabry22 <Muhammad2602095@fci.sohag.edu.eg>
2025-11-11 11:23:35 +0200

    change theme & notes view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:_10_note_app/views/notes_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      debugShowCheckedModeBanner: false,
+      home: const NotesView(),
+    );
   }
 }
