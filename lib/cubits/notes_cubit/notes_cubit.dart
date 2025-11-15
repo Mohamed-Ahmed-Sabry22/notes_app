65a0b02eaf467d88544b656502fd61d852df9de4
Mohamed-Ahmed-Sabry22 <Muhammad2602095@fci.sohag.edu.eg>
2025-11-15 16:04:10 +0200

    create notes state

---
+++ lib/cubits/notes_cubit/notes_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:_10_note_app/models/note_model.dart';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit() : super(NotesInitial());
+}
